435cba7b319dd5cb05d6dac65835ee18c6974282
ajasniew <alina.jasniewski@stonybrook.edu>
2016-10-31 12:11:01 -0400

    Indentations

--- js/annotationtools/geoJSONHandlerTypePoint.js
+++ js/annotationtools/geoJSONHandlerTypePoint.js
@@ -117,51 +117,52 @@ annotools.prototype.generateCircleSVG = function(annotationId, nativepoints, ann
 	
     var response = annotools.getProperties(annotationId);
     var result = JSON.parse(response.responseText);
-	var currentRadius = result[0].properties.radius;
+    var currentRadius = result[0].properties.radius;
     var hoverRadius = currentRadius * 4;
     var fillColor = result[0].properties.fill_color;
     var hoverColor = result[0].properties.fill_color;
-	var region = result[0].properties.annotations.region;
-	var additionalAnnotation = result[0].properties.annotations.additional_annotation;
-	var additionalNotes = result[0].properties.annotations.additional_notes;
+    var region = result[0].properties.annotations.region;
+    var additionalAnnotation = result[0].properties.annotations.additional_annotation;
+    var additionalNotes = result[0].properties.annotations.additional_notes;
     var lymphocyteRegion = 'Lymphocyte';
 	
-	var text = region;
+    var text = region;
 
-	/*
+    /*
     console.log(JSON.stringify(result, null, 4));
 	console.log(result[0].properties.annotations.region);
 	console.log(result[0].properties.annotations.additional_annotation);
 	console.log(result[0].properties.annotations.additional_notes);
 	console.log(result[0].properties.radius);
 	console.log(result[0].properties.fill_color);
-	*/
+    */
 	
     if (region === lymphocyteRegion) {
         fillColor = 'lime';
         hoverColor = 'lime';
     }
 	
-	if (currentRadius === undefined) {
-		currentRadius = 3;
-	}
+    if (currentRadius === undefined) {
+        currentRadius = 3;
+    }
 	
-	if (fillColor === undefined) {
-		fillColor = '#ffff00';
-		hoverColor = '#ffff00';
-	}
+    if (fillColor === undefined) {
+        fillColor = '#ffff00';
+        hoverColor = '#ffff00';
+    }
 	
-	if (text) {
+    if (text) {
 	
-		if (additionalAnnotation) {
-			text += '\n';
-		    text += additionalAnnotation;
-		}
-		if (additionalNotes) {
-			text += '\n';
-			text += additionalNotes;
-		}	
-	}
+        if (additionalAnnotation) {
+            text += '\n';
+            text += additionalAnnotation;
+        }
+		
+        if (additionalNotes) {
+            text += '\n';
+            text += additionalNotes;
+        }	
+    }
     
     for (var k = 0; k < nativepoints.length; k++) {
 
